3dacdb46ae3eef4cd5d56b1884d8280da3589e87
mahen111 <152676258+mahen111@users.noreply.github.com>
2023-12-11 10:19:13 +0530

    Add files via upload

---
+++ map inbuilt functuion.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
+squared = [2]
+#for num in numbers:
+numbers.append(squared)
+numbers1 = [2, 4, 6, 4, 10]
+squared = [3]
+print(squared)
+numbers2 = ("ram", "sita", "bharat")
+squared.sort(1)
+peint(squared)
